d2a51bdb8d270efb3afed30900fcdd0757d1740a
Robinson Santana Silverio <robinsntn@gmail.com>
2019-10-07 17:50:21 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,17 +1,4 @@
-scale = [2,3,4,5]
-scale_2 = [6,5,4,3]
-hill = [1,2,3,4,5,4,3,2,1]
-valley = [5,4,3,2,1,0,1,2,3,4,5]
-zig_zag = [500,4,1000,5,250]
-
-def find_element_index(array, value_to_find)
+def find_max_value(array, value_to_find)
   array.index(value_to_find)
 end
 
-def find_max_value(array)
-  array.max
-end
-
-def find_min_value(array)
-  array.min
-end
